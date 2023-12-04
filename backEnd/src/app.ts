b2cfa25d2d0f1e0daa55f6fcaf1d4fcb4cb8b150
ketlynledriane <ketlyn-ledriane@hotmail.com>
2023-12-04 15:25:32 -0300

    Removendo o sync no backend

--- backEnd/src/app.ts
+++ backEnd/src/app.ts
@@ -2,7 +2,9 @@ import DB from "./db";
 import server from "./server";
 
 async function main(): Promise<void> {
-  await (await DB.initialize()).synchronize();
+  let db = await DB.initialize();
+  
+  // await db.synchronize();
 
   server.start();
 }
